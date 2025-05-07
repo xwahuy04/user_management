66c301b4ceafbbe10ef1880c5f7038255c1a77b7
xwahuy04 <wahyuhavidbaihaqi@gmail.com>
2025-05-07 15:00:26 +0700

    first commit(not yet)

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+
+Route::prefix('user/')->group(function (){
+ Route::get('list' , [UserController::class, 'index']);
+ Route::get('create' , [UserController::class, 'create']);
+ Route::post('store' , [UserController::class, 'store']);
+ Route::get('edit/{id}' , [UserController::class, 'edit']);
+ Route::post('update' , [UserController::class, 'update']);
+ Route::get('delete/{id}' , [UserController::class, 'delete']);
+ // Route::get('search' , [UserController::class, 'search']);
+ Route::get('show/{id}' , [UserController::class, 'show']);
+});
